91bf57c38dc7eb33dc735aeee1327b0c8c9598af
Peter Jennings <peter@localhost.localdomain>
2015-01-17 09:37:38 -0800

    Hello world example

---
+++ hello_world.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_world.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/17/2015 09:34:54 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, char argv[])
+{
+	printf("Hello world.\n");
+}
